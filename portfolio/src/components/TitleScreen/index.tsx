09ecc85f722c2569c35f9386f5ba78d6ff642118
Andrew Bass <andrewbass@Andrews-MacBook-Pro.local>
2023-11-18 17:23:23 -0500

    wip

--- portfolio/src/components/TitleScreen/index.tsx
+++ portfolio/src/components/TitleScreen/index.tsx
@@ -7,16 +7,22 @@ export const TitleScreen: React.FC = () => {
   const titleRef = React.useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
-    const tl = gsap.timeline({repeat: -1});
+    const tl = gsap.timeline({repeat: -1, yoyo: true});
     tl.to(titleRef.current, {
-      scale: 3,
-      duration: 3,
+      scale: 1.25,
+      ease: "sine.out",
+      duration: 1.6,
     });
   });
 
     return (
       <div className='title-screen-wrapper'>
-        <p className="title-text" ref={titleRef}>click to enter.</p>
+        <svg className='circle-wrapper' width="200" height="200">
+          <circle id="circle1" cx="100" cy="100" r="10" fill="grey"/>
+        </svg>
+        <p className="title-text" ref={titleRef}>
+          click to enter
+        </p>
       </div>
     );
 }
