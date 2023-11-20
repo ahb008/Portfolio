21d3248b0359f3c4f0b734d42e227cbdf51242cc
Andrew Bass <andrewbass@Andrews-MacBook-Pro.local>
2023-11-20 11:56:14 -0500

    click function

--- portfolio/src/components/TitleScreen/index.tsx
+++ portfolio/src/components/TitleScreen/index.tsx
@@ -1,26 +1,42 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from "gsap";
 import './titleScreen.scss';
 
 export const TitleScreen: React.FC = () => {
 
-  const titleRef = React.useRef<HTMLInputElement>(null);
+  const [reversed, setReversed] = useState(false);
+  const tl = useRef();
+
+  const clickFunction = () => {
+    console.log("yo");
+    // clickAnimation();
+  }
 
   useLayoutEffect(() => {
-    const tl = gsap.timeline({repeat: -1, yoyo: true});
-    tl.to(titleRef.current, {
-      scale: 1.25,
-      ease: "sine.out",
-      duration: 1.6,
-    });
-  });
+    const ctx = gsap.context(() => {
+      // @ts-ignore
+      tl.current = gsap
+        .timeline({repeat: -1, yoyo: true})
+        .to(".title-text", {
+            scale: 1.25,
+            ease: "sine.out",
+            duration: 1.6,
+          });
+      });
+
+      // Animation cleanup function
+      return () => ctx.revert();
+    }, []);
+
+    useEffect(() => {
+      console.log("toggling reverse to", reversed);
+      // @ts-ignore
+      tl.current?.reversed(reversed);    
+    }, [reversed]);
 
     return (
       <div className='title-screen-wrapper'>
-        <svg className='circle-wrapper' width="200" height="200">
-          <circle id="circle1" cx="100" cy="100" r="10" fill="grey"/>
-        </svg>
-        <p className="title-text" ref={titleRef}>
+        <p className="title-text" onClick={() => setReversed(!reversed)}>
           click to enter
         </p>
       </div>
